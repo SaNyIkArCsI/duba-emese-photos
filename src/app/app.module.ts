a2ba8739b2712a4ca7611db566c0a7ba86ff124a
sanyikarcsi <sanyikarcsi@gmail.com>
2018-11-10 18:30:37 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SlideshowComponent } from './home/slideshow/slideshow.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PrivacyStatementComponent } from './footer/privacy-statement/privacy-statement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SlideshowComponent,
+    AboutComponent,
+    ContactComponent,
+    GalleryComponent,
+    FooterComponent,
+    NavigationBarComponent,
+    ProjectsComponent,
+    PrivacyStatementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
